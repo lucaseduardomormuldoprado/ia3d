1776a316d87547f4ffed856965884b303afe9532
lucas <110677140+lucaseduardomormuldoprado@users.noreply.github.com>
2024-08-23 07:48:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,24 +3,25 @@ const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
 
-const perguntas = [//abre a lista de objetos (itens)
-    {//abre o item
-        enunciado:"Pergunta 1",
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
         alternativas:["Alternativa 1","Alternativa 2"]
-    }
-    {//abre o item
-        enunciado:"Pergunta 1",
+    },
+    {
+        enunciado: "Pergunta 2",
         alternativas:["Alternativa 1","Alternativa 2"]
-    }
-    {//abre o item
-        enunciado:"Pergunta 1",
+    },
+    {
+        enunciado: "Pergunta 3",
         alternativas:["Alternativa 1","Alternativa 2"]
     }
 ]
 
-let atual = 0
+let atual = 0;
 let perguntaAtual;
 
-function mostrapergunta(){
-    
+function mostraPergunta(){
+    pertguntaAtual=perguntas[atual];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
 }
